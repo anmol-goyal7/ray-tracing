0f0539a26f91f153f4489169d812a8697e293d24
anmol <anmolafk7@gmail.com>
2026-03-02 03:07:05 +0530

    added multiple mathematics operations

--- experiments/mini-renderer/src/vec3.h
+++ experiments/mini-renderer/src/vec3.h
@@ -31,7 +31,7 @@ public:
           e[2] + other.e[2]
           );
     } 
-     Vec3 operator*(const Vec3& other) const {             
+    Vec3 operator*(const Vec3& other) const {             
       return Vec3(                                            
           e[0] * other.e[0],                              
           e[1] * other.e[1],                              
@@ -39,12 +39,32 @@ public:
           );                                              
   
     }
+    Vec3 operator/(double t) const {
+      return Vec3(
+          e[0]/t,
+          e[1]/t,
+          e[2]
+          )
+    }
 };
 
- inline double dot(const Vec3& a, const Vec3& b) {
+inline double dot(const Vec3& a, const Vec3& b) {
        return a.e[0] * b.e[0] +
               a.e[1] * b.e[1] +
               a.e[2] * b.e[2]; 
      }
 
-                                                     
+double length_squared() const { 
+  return e[0]*e[0]+
+         e[1]*e[1]+
+         e[2]*e[2];
+} 
+
+double length() const {
+  return std::sqrt(length_squared());
+}
+
+inline Vec3 unit_vector(const Vec3& v ) {
+  return v / v.length();
+}
+

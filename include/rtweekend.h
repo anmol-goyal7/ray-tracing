095481dd9501cbeb6e1c4d9599414ed689217fd1
anmol <anmolafk7@gmail.com>
2026-06-18 00:10:10 +0530

    fix random double helper

--- include/rtweekend.h
+++ include/rtweekend.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdlib>
 #include <cmath>
 #include <iostream>
 #include <memory>
@@ -13,6 +14,14 @@ inline double degrees_to_radius(double degrees) {
     return degrees * pi / 180.0;
 }
 
+inline double random_double() {
+    //returns a random real number in (0,1).
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    return min + (max - min) * random_double();
+}
 #include "color.h"
 #include "ray.h"
 #include "vec3.h"
